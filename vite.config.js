a6222e756f61b19787d5f3aa96cbc321351b8e3e
Sydorov Artem <sydorovay@gmail.com>
2025-01-14 20:16:46 +0100

    add fortawesome

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows connection from any device on the network
+    port: 5173, 
+  },
 })
