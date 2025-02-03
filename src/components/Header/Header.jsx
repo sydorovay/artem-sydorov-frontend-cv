515091d0d923553f70b3851be41a974c168136f7
Sydorov Artem <sydorovay@gmail.com>
2025-02-03 11:39:33 +0100

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <Container >
       <header className={styles.header} >
         <h1 className={styles.caption}>Artem Sydorov</h1>
-        <p className={styles.paragraph}>Welcome to my profession resume </p>
+        <p className={styles.paragraph}>Welcome to my profession resume</p>
       </header>
     </Container>
   )
