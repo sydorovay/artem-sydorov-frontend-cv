df6b59f8ee69e3809370a632cba8ec98429b039e
Sydorov Artem <sydorovay@gmail.com>
2025-02-05 21:43:16 +0100

    add style for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,9 +5,9 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <Container >
-    <footer className={styles.footer}>
-      <p>@2025 My CV. All rights reserved.</p>
-      <p>Conact me: sydorovay@gmail.com</p>
+      <footer className={styles.footer}>
+        <p className={styles.email}>Conact me: sydorovay@gmail.com</p>
+        <p className={styles.license}>@2025 My CV. All rights reserved.</p>
       </footer> 
     </Container>
   )
