1288158ed179a7c804f80447166951f152263f21
Sydorov Artem <sydorovay@gmail.com>
2025-03-02 21:01:10 +0100

    update style

--- src/components/sections/SkillsSection/Languages/Languages.jsx
+++ src/components/sections/SkillsSection/Languages/Languages.jsx
@@ -7,20 +7,21 @@ const Languages = () => {
     <section className={styles.languages}>
       <h2>Languages</h2>
         <ul className={styles.list}>
+          
+          <li className={styles.item}>
+            <span className={styles.language}>Polish</span>: <span className={styles.level}>Basic (A2)</span>
+          </li>
+          <li className={styles.item}>
+            <span className={styles.language}>English</span>: <span className={styles.level}>Basic (A2)</span>
+          </li>
+          <li className={styles.item}>
+            <span className={styles.language}>German</span>: <span className={styles.level}>Basic (A2)</span>
+          </li>
           <li className={styles.item}>
             <span className={styles.language}>Ukrainian</span>: <span className={styles.level}>Native</span>
           </li>
           <li className={styles.item}>
             <span className={styles.language}>Russian</span>: <span className={styles.level}>Proficient (C2)</span>
-          </li>
-          <li className={styles.item}>
-            <span className={styles.language}>Polish</span>: <span className={styles.level}>Intermediate (B1)</span>
-          </li>
-          <li className={styles.item}>
-            <span className={styles.language}>English</span>: <span className={styles.level}>Intermediate (B1)</span>
-          </li>
-          <li className={styles.item}>
-            <span className={styles.language}>German</span>: <span className={styles.level}>Basic (A2)</span>
           </li>
         </ul>
       </section>
