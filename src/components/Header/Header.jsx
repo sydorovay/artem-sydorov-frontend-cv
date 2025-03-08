7393eb4861cfb2ff224e004582d59182e7a8531f
Sydorov Artem <sydorovay@gmail.com>
2025-03-08 19:43:08 +0100

    create navigation component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,40 +1,31 @@
-import { useState } from 'react'
-import Container from '../Container/Container'
-import styles from './Header.module.css'
+import { useState } from 'react';
+import Container from '../Container/Container';
+import Navigation from '../Navigation/Navigation';
+import styles from './Header.module.css';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  
+
   const toggleMenu = () => {
     setMenuOpen((prev) => !prev);
-
-}
+  };
 
   return (
-    
     <header className={styles.header}>
-      <Container  className={styles.headerContainer}>
+      <Container className={styles.headerContainer}>
         <div className={styles.container}>
           <h1 className={styles.logo}>Artem Sydorov</h1>
-          <p className={styles.paragraph}>Welcome to my profession resume</p>
+          <p className={styles.paragraph}>Welcome to my professional resume</p>
           <button className={styles.burger} onClick={toggleMenu}>
             <span className={menuOpen ? styles.lineOpen : styles.line}></span>
             <span className={menuOpen ? styles.lineOpen : styles.line}></span>
             <span className={menuOpen ? styles.lineOpen : styles.line}></span>
           </button>
-
-          <nav className={`${styles.nav} ${menuOpen ? styles.open : ''}`}>
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/about">About me</a></li>
-              <li><a href="/contact">Contacts</a></li>
-            </ul>
-          </nav>
+          <Navigation menuOpen={menuOpen} />
         </div>
       </Container>
-      </header>
-  )
+    </header>
+  );
 };
 
-
-export default Header
+export default Header;
